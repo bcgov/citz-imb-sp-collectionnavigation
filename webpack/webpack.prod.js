38e8ac088523ae9599fddee1a8db75bcc01f0590
SDToews <33942967+SDToews@users.noreply.github.com>
2021-04-15 10:40:36 -0700

    webpack analyzer

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
 	mode: 'production',
 	devtool: 'source-map',
-	plugins: [],
+	plugins: [new BundleAnalyzerPlugin()],
 };
